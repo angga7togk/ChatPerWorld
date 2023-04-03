23bebf2c16fa62c8e3045a010d57cf962dbf5b51
angga7togk <90005225+angga7togk@users.noreply.github.com>
2023-04-03 14:00:34 +0700

    GlobalChat

--- src/angga7togk/chatperworld/ChatPerWorld.php
+++ src/angga7togk/chatperworld/ChatPerWorld.php
@@ -7,13 +7,20 @@ use pocketmine\utils\Config;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\console\ConsoleCommandSender;
 
 class ChatPerWorld extends PluginBase implements Listener{
 
     public $cfg;
+    public $prefix;
 
     public function onEnable():void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+        $this->prefix = $this->cfg->get("Prefix")." ";
     }
 
     public function onChat(PlayerChatEvent $event){
@@ -27,5 +34,18 @@ class ChatPerWorld extends PluginBase implements Listener{
 			}
 		}
 		$event->setRecipients($recipients);
+    }
+
+    public function onCommand(CommandSender $player, Command $cmd, String $label, Array $args):bool{
+        if($cmd->getName() == "globalchat"){
+            if(isset($args[0])){
+                $player->getServer()->broadcastMessage("".str_replace(["{player}", "{msg}"], [$player->getName(), implode(" ", $args)], $this->cfg->get("Format")));
+                return true;
+            }else{
+                $player->sendMessage($this->prefix."Usage: /gchat <message>");
+                return true;
+            }
+        }
+        
     }
 }
